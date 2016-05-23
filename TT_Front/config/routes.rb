37921413a43aea6d8c8b0fe14c46b026b3d25939
Kevin Trizna <triznakj@rose-hulman.edu>
2016-05-23 18:32:34 -0400

    Rails is online and node server has been created

--- TT_Front/config/routes.rb
+++ TT_Front/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +15,13 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  Rails.application.routes.draw do
+
+    resources :users
+
+    root 'home#index'
+  end
 
   # Example resource route with options:
   #   resources :products do
